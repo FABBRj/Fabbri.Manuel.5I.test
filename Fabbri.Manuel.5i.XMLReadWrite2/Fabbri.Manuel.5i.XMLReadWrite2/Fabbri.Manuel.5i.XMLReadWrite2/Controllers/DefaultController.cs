68b7609285e345090fb0d9d1c4bf236c72fcc050
FABBRj <fabbrjmanuel@gmail.com>
2017-04-11 10:34:37 +0200

    Fabbri.Manuel.5I.XMLReadWrite2

---
+++ Fabbri.Manuel.5i.XMLReadWrite2/Fabbri.Manuel.5i.XMLReadWrite2/Fabbri.Manuel.5i.XMLReadWrite2/Controllers/DefaultController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace Fabbri.Manuel._5i.XMLReadWrite2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        
+
+        /// <summary>
+        /// ///
+        /// </summary>
+
+        private string nomeFile = HostingEnvironment.MapPath(@"~/App_Data/Persone.xml");
+
+        public ActionResult Index()
+        {
+            XElement data = XElement.Load(nomeFile);
+            var persone = (from l in data.Elements("Persona") select new Persona(l)).ToList();
+            return View(persone);
+        }
+
+        public ActionResult XMLReadWrite(int nn = 0)
+        {
+            var p = new Persone(nomeFile);
+
+            if (nn != 0)
+                p.Aggiungi();
+
+            return View(p);
+        }
+
+        public ActionResult AddPredefinito()
+        {
+            var p = new Persone(nomeFile);
+            p.Aggiungi();
+
+            return View("XMLReadWrite", p);
+        }
+
+        public ActionResult DelSelected()
+        {
+            var p = new Persone(nomeFile);
+            p.Aggiungi();
+
+            return View("XMLReadWrite", p);
+        }
+    }
+}
---
+++ Fabbri.Manuel.5i.XMLReadWrite2/Fabbri.Manuel.5i.XMLReadWrite2/Fabbri.Manuel.5i.XMLReadWrite2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fabbri.Manuel._5i.XMLReadWrite2.MvcApplication" Language="C#" %>
